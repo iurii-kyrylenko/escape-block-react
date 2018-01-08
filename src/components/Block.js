bc6f818082244abda95d472daa9dfe2632cc7493
iurii-kyrylenko <yuriproject@gmail.com>
2018-01-08 15:54:13 +0200

    added state to board

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { CELL } from '../const'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { CELL } from '../const';
 
 class Block extends Component {
   render () {
-    const { dir, len, row, pos, target } = this.props
-    const style = {
-      width: dir === 'v' ? CELL : len * CELL,
-      height: dir === 'v' ? len * CELL : CELL,
-      top: dir === 'v' ? pos * CELL : row * CELL,
-      left: dir === 'v' ? row * CELL : pos * CELL
-    }
-    const className = 'block' + (target ? ' target' : '')
+    const { dir, len, row, pos, target } = this.props;
+
+    const style = dir === 'v'
+      ? { width: CELL, height: len * CELL, top: pos * CELL, left: row * CELL }
+      : { width: len * CELL, height: CELL, top: row * CELL, left: pos * CELL };
+
+    const className = 'block' + (target ? ' target' : '');
+
     return (
       <div className={className} style={style} />
-    )
+    );
   }
 }
 
@@ -24,10 +24,10 @@ Block.propTypes = {
   row: PropTypes.number,
   pos: PropTypes.number,
   target: PropTypes.bool
-}
+};
 
 Block.defaultProps = {
   target: false
-}
+};
 
-export default Block
+export default Block;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,60 @@
-import React, { Component } from 'react'
-import Block from './Block'
-import styles from './Board.css'
-import { CELL, COLS, ROWS } from '../const'
+// @flow
 
-const Board = () => (
-  <div className="board" style={{ width: COLS * CELL, height: ROWS * CELL }}>
-    <Block {...{ dir: 'h', len: 2, row: 1, pos: 1 }} />
-    <Block {...{ dir: 'h', len: 2, row: 2, pos: 2, target: true }} />
-    <Block {...{ dir: 'h', len: 3, row: 3, pos: 0 }} />
-    <Block {...{ dir: 'h', len: 2, row: 5, pos: 0 }} />
-    <Block {...{ dir: 'h', len: 2, row: 5, pos: 3 }} />
-    <Block {...{ dir: 'v', len: 3, row: 0, pos: 0 }} />
-    <Block {...{ dir: 'v', len: 2, row: 2, pos: 4 }} />
-    <Block {...{ dir: 'v', len: 2, row: 3, pos: 0 }} />
-    <Block {...{ dir: 'v', len: 3, row: 4, pos: 2 }} />
-    <Block {...{ dir: 'v', len: 2, row: 5, pos: 0 }} />
-    <Block {...{ dir: 'v', len: 2, row: 5, pos: 3 }} />
-    <div className="exit" style={{ width: 4, height: CELL + 2, top: 2 * CELL - 1, left: COLS * CELL }}></div>
-    </div>
-)
+import React, { Component } from 'react';
+import Block from './Block';
+import styles from './Board.css';
+import { CELL, COLS, ROWS } from '../const';
 
-export default Board
+type BlockType = {
+  dir: 'h' | 'v',
+  len: number,
+  row: number,
+};
+
+type BoardStateType = {
+  board: Array<BlockType>,
+  state: Array<number>,
+  target: number,
+};
+
+class Board extends Component<{}, BoardStateType> {
+  state = {
+    board: [
+      { dir: 'h', len: 2, row: 1 },
+      { dir: 'h', len: 2, row: 2 },
+      { dir: 'h', len: 3, row: 3 },
+      { dir: 'h', len: 2, row: 5 },
+      { dir: 'h', len: 2, row: 5 },
+      { dir: 'v', len: 3, row: 0 },
+      { dir: 'v', len: 2, row: 2 },
+      { dir: 'v', len: 2, row: 3 },
+      { dir: 'v', len: 3, row: 4 },
+      { dir: 'v', len: 2, row: 5 },
+      { dir: 'v', len: 2, row: 5 }
+    ],
+    state: [1,2,0,0,3,0,4,0,2,0,3],
+    target: { index: 1, position: 4 },
+  };
+
+  render() {
+    const { board, state, target } = this.state;
+    const targetBlock = board[target.index];
+    const gatePos = dir => target.position === 0 ? -4 : (dir === 'h' ? COLS : ROWS) * CELL;
+    const gateHeight = targetBlock.row === ROWS - 1 ? CELL + 2 : CELL + 3;
+    const gateWidth = targetBlock.row === COLS - 1 ? CELL + 2 : CELL + 3;
+    const gateStyle = targetBlock.dir === 'h'
+      ? { width: 4, height: gateHeight, top: targetBlock.row * CELL - 1, left: gatePos('h') }
+      : { width: gateWidth, height: 4, top: gatePos('v'), left: targetBlock.row * CELL - 1 };
+
+    return (
+      <div className="board" style={{ width: COLS * CELL, height: ROWS * CELL }}>
+        {board.map((block, id) => (
+            <Block {...block} pos={state[id]} target={target.index === id} />
+        ))}
+        <div className="exit" style={gateStyle}></div>
+      </div>
+    );
+  }
+}
+
+export default Board;
